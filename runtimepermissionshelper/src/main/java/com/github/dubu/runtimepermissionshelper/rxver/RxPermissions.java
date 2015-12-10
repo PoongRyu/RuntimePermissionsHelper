9c13a7e19612b50683020006d1c977d8ef785b64
DUBU <dubudev@gmail.com>
2015-12-10 16:25:42 +0900

    update

--- runtimepermissionshelper/src/main/java/com/github/dubu/runtimepermissionshelper/rxver/RxPermissions.java
+++ runtimepermissionshelper/src/main/java/com/github/dubu/runtimepermissionshelper/rxver/RxPermissions.java
@@ -1,17 +1,3 @@
-/**
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.github.dubu.runtimepermissionshelper.rxver;
 
 import android.annotation.TargetApi;
@@ -77,7 +63,7 @@ public class RxPermissions {
                 .flatMap(new Func1<Object, Observable<Permission>>() {
                     @Override
                     public Observable<Permission> call(Object o) {
-                        return request_(permissions);
+                        return requestPermission(permissions);
                     }
                 });
     }
@@ -124,7 +110,7 @@ public class RxPermissions {
     }
 
     @TargetApi(Build.VERSION_CODES.M)
-    private Observable<Permission> request_(final String... permissions) {
+    private Observable<Permission> requestPermission(final String... permissions) {
         if (mLogging) {
             log("Requesting permissions " + TextUtils.join(", ", permissions));
         }
